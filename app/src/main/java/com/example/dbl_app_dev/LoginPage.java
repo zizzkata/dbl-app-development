209dfcfe1815a9675c69739cbc292a6cea9eb09f
foxnase <82893286+foxnase@users.noreply.github.com>
2022-03-15 15:30:07 +0100

    Login Page and Sign up Page
    Created Login Page and Sign Up page with interactivity going from one page to the other. TODO: Add scrolling

---
+++ app/src/main/java/com/example/dbl_app_dev/LoginPage.java
@@ -0,0 +1,26 @@
+package com.example.dbl_app_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        //Sign Up button leading to RegisterPage
+        TextView btn=findViewById(R.id.signUpButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginPage.this,RegisterPage.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dbl_app_dev/RegisterPage.java
@@ -0,0 +1,29 @@
+package com.example.dbl_app_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.dbl_app_dev.LoginPage;
+
+public class RegisterPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+
+        //Log in button leading to LoginPage
+        TextView btn=findViewById(R.id.logInButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(com.example.dbl_app_dev.RegisterPage.this,
+                        LoginPage.class));
+            }
+        });
+    }
+}
