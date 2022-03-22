0cba815b8686fd606abb27d9208379a2af6a778a
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-03-22 17:43:17 +0100

    added switch that changes the nav bar icons based on the mode

--- app/src/main/java/com/example/dbl_app_dev/LandlordDiscoverFragment.java
+++ app/src/main/java/com/example/dbl_app_dev/LandlordDiscoverFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link tenantDiscoverFragment#newInstance} factory method to
+ * Use the {@link LandlordDiscoverFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class tenantDiscoverFragment extends Fragment {
+public class LandlordDiscoverFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class tenantDiscoverFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public tenantDiscoverFragment() {
+    public LandlordDiscoverFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class tenantDiscoverFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment secondFragment.
+     * @return A new instance of fragment landlordDiscoverFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static tenantDiscoverFragment newInstance(String param1, String param2) {
-        tenantDiscoverFragment fragment = new tenantDiscoverFragment();
+    public static LandlordDiscoverFragment newInstance(String param1, String param2) {
+        LandlordDiscoverFragment fragment = new LandlordDiscoverFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class tenantDiscoverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tenant_discover, container, false);
+        return inflater.inflate(R.layout.fragment_landlord_discover, container, false);
     }
 }
--- app/src/main/java/com/example/dbl_app_dev/TenantLikedFragment.java
+++ app/src/main/java/com/example/dbl_app_dev/TenantLikedFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link settingsFragment#newInstance} factory method to
+ * Use the {@link TenantLikedFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class settingsFragment extends Fragment {
+public class TenantLikedFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class settingsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public settingsFragment() {
+    public TenantLikedFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class settingsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment thirdFragment.
+     * @return A new instance of fragment firstFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static settingsFragment newInstance(String param1, String param2) {
-        settingsFragment fragment = new settingsFragment();
+    public static TenantLikedFragment newInstance(String param1, String param2) {
+        TenantLikedFragment fragment = new TenantLikedFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class settingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_tenant_liked, container, false);
     }
 }
