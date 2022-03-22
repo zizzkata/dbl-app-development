9657e07c4a35aa5d48380a0212fc26a6219e54f1
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-03-22 15:45:24 +0100

    added basic nav bar functionality and made it so selected icons change

--- app/src/main/java/com/example/dbl_app_dev/MainNavigationActivity.java
+++ app/src/main/java/com/example/dbl_app_dev/MainNavigationActivity.java
@@ -1,8 +1,15 @@
 package com.example.dbl_app_dev;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainNavigationActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainNavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
+
+        linkNavControllerToNavBar();
+    }
+
+    /**
+     * Method used for linking the navigation controller to the navigation bar
+     */
+    private void linkNavControllerToNavBar() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
