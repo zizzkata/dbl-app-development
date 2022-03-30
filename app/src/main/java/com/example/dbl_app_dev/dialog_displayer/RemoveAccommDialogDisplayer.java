550aae5790744b63b53430c8e3b258f950525289
Kolev <56160005+KolevVelyan@users.noreply.github.com>
2022-03-30 12:26:47 +0200

    changed remove dialog to have configurable title and message

--- app/src/main/java/com/example/dbl_app_dev/dialog_displayer/RemoveAccommDialogDisplayer.java
+++ app/src/main/java/com/example/dbl_app_dev/dialog_displayer/RemoveAccommDialogDisplayer.java
@@ -1,6 +1,7 @@
 package com.example.dbl_app_dev.dialog_displayer;
 
 import android.content.Context;
+import android.media.MediaSession2Service;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -13,10 +14,21 @@ import com.example.dbl_app_dev.R;
  */
 public class RemoveAccommDialogDisplayer extends DialogDisplayer {
     View removedView;
+    String title = "";
+    String message = "";
 
     public RemoveAccommDialogDisplayer(Context context, View removedView) {
         super(context, -1, -1, null);
         this.removedView = removedView;
+        this.title = context.getString(R.string.remove_listing_title);
+        this.message = context.getString(R.string.remove_listing_message);
+    }
+
+    public RemoveAccommDialogDisplayer(Context context, View removedView, String title, String message) {
+        super(context, -1, -1, null);
+        this.removedView = removedView;
+        this.title = title;
+        this.message = message;
     }
 
     @Override
@@ -28,8 +40,8 @@ public class RemoveAccommDialogDisplayer extends DialogDisplayer {
     protected void setDialogProperties() {
         dialog.setCancelable(false);
         dialog.setIcon(R.drawable.ic_warning_filled);
-        dialog.setTitle("Remove listing");
-        dialog.setMessage("Are you sure you want to permanently remove listing from liked page!");
+        dialog.setTitle(title);
+        dialog.setMessage(message);
         dialog.setButton(dialog.BUTTON_NEGATIVE, "Cancel",
                 (dialog, which) -> dialog.dismiss());
         dialog.setButton(dialog.BUTTON_POSITIVE, "Remove",
