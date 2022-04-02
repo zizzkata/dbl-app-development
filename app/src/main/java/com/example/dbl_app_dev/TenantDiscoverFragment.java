266f3b24e96093aca6adee84d5b547960b97eb5d
Macovei Tudor <tudor_macovei@iCloud.com>
2022-04-02 14:48:11 +0200

    Modify card data model to work with current UI

--- app/src/main/java/com/example/dbl_app_dev/TenantDiscoverFragment.java
+++ app/src/main/java/com/example/dbl_app_dev/TenantDiscoverFragment.java
@@ -21,6 +21,8 @@ import androidx.viewpager.widget.ViewPager;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
 import java.util.LinkedList;
 
 /**
@@ -36,7 +38,8 @@ public class TenantDiscoverFragment extends Fragment implements SwipeHandler {
     VerticalViewPager imageGalleryViewPager;
 
     public TenantDiscoverFragment() {
-        /* Required empty public constructor */ }
+        /* Required empty public constructor */
+    }
 
     /**
      * Use this factory method to create a new instance of
@@ -58,89 +61,105 @@ public class TenantDiscoverFragment extends Fragment implements SwipeHandler {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tenant_discover, container, false);
     }
-/**
+
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         ConstraintLayout topCard = view.findViewById(R.id.topCard);
-        TextView nameTxt = view.findViewById(R.id.accommodationName);
-        TextView descriptionTxt = view.findViewById(R.id.accommodationDescription);
+        ArrayList<TextView> cardTextViews = new ArrayList<>();
+        cardTextViews.add(view.findViewById(R.id.addressTxt));
+        cardTextViews.add(view.findViewById(R.id.floorTxt));
+        cardTextViews.add(view.findViewById(R.id.postcodeTxt));
+        cardTextViews.add(view.findViewById(R.id.priceTxt));
+        cardTextViews.add(view.findViewById(R.id.accommTypeTxt));
+        cardTextViews.add(view.findViewById(R.id.utilitiesTxt));
+        cardTextViews.add(view.findViewById(R.id.areaTxt));
+        cardTextViews.add(view.findViewById(R.id.furnishedTxt));
+        cardTextViews.add(view.findViewById(R.id.petsTxt));
+        cardTextViews.add(view.findViewById(R.id.smokersTxt));
+        cardTextViews.add(view.findViewById(R.id.minimumPeriodTxt));
+        cardTextViews.add(view.findViewById(R.id.availableTxt));
+        cardTextViews.add(view.findViewById(R.id.untilTxt));
+        cardTextViews.add(view.findViewById(R.id.descriptionTxt));
 
-        imageGalleryViewPager = view.findViewById(R.id.accommodationImageScroller);
         imageGalleryViewPager = view.findViewById(R.id.accommodationImageScroller);
 
         this.horizontalSwipeDetector = new GestureDetector(getContext(), new CardSwipeListener(this, true, false));
         topCard.setOnTouchListener((v, event) -> {
             if (horizontalSwipeDetector.onTouchEvent(event)) {
-                nextCard(nameTxt, imageGalleryViewPager, descriptionTxt);
+                nextCard(cardTextViews, imageGalleryViewPager);
                 return false;
             }
             return true;
         });
 
-        Button likeBtn = view.findViewById(R.id.accommodationLikeBtn);
-        Button dislikeBtn = view.findViewById(R.id.accommodationDislikeBtn);
+        Button likeBtn = view.findViewById(R.id.positiveButton);
+        Button dislikeBtn = view.findViewById(R.id.negativeButton);
 
         likeBtn.setOnClickListener(v -> {
             swipedRight();
-            nextCard(nameTxt, imageGalleryViewPager, descriptionTxt);
+            nextCard(cardTextViews, imageGalleryViewPager);
         });
         dislikeBtn.setOnClickListener(v -> {
             swipedLeft();
-            nextCard(nameTxt, imageGalleryViewPager, descriptionTxt);
+            nextCard(cardTextViews, imageGalleryViewPager);
         });
 
-        imageGalleryViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                // mandatory override
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                Log.d("API123", "onPageSelected " + position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                // mandatory override
-            }
-        });
+//        imageGalleryViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                // mandatory override
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                Log.d("API123", "onPageSelected " + position);
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//                // mandatory override
+//            }
+//        });
 
         // makes sure that the a card is not discarded if it is not rated
         if (currentAccommodationInfo == null) {
-            nextCard(nameTxt, imageGalleryViewPager, descriptionTxt);
+            nextCard(cardTextViews, imageGalleryViewPager);
         } else {
-            displayCard(nameTxt, imageGalleryViewPager, descriptionTxt);
+            displayCard(cardTextViews, imageGalleryViewPager);
         }
 
     }
- */
+
     /**
      * Updates the information on currentAccommodationInfo and displays it
      */
-    private void nextCard(TextView cardTitle, VerticalViewPager imageGalleryViewPager, TextView cardDescription) {
+    private void nextCard(ArrayList<TextView> cardTextViews, VerticalViewPager imageGalleryViewPager) {
         if (dataModels.size() > 0) {
             currentAccommodationInfo = this.dataModels.remove();
-            displayCard(cardTitle, imageGalleryViewPager, cardDescription);
+            displayCard(cardTextViews, imageGalleryViewPager);
         } else {
-            cardDescription.setText("...");
-            cardTitle.setText("No more swipes in your area");
+            // no more swipes left
+//            cardDescription.setText("...");
+//            cardTitle.setText("No more swipes in your area");
         }
     }
 
     /**
      * Displays the information stored in currentAccommodationInfo
      */
-    private void displayCard(TextView cardTitle, VerticalViewPager imageGalleryViewPager, TextView cardDescription) {
-        cardTitle.setText(currentAccommodationInfo.getFormattedName());
-        cardDescription.setText(currentAccommodationInfo.getDescription());
+    private void displayCard(ArrayList<TextView> cardTextViews, VerticalViewPager imageGalleryViewPager) {
+        ArrayList<String> cardStrings = currentAccommodationInfo.getCardFormattedText();
+        assert (cardStrings.size() == cardTextViews.size()) : "Incorrect size of accommodation info strings";
+        for (int i = 0; i < cardStrings.size(); i++) {
+            cardTextViews.get(i).setText(cardStrings.get(i));
+        }
         imageGalleryViewPager
                 .setAdapter(new ImageViewPagerAdapter(getChildFragmentManager(), currentAccommodationInfo.getPhotos()));
     }
@@ -152,14 +171,18 @@ public class TenantDiscoverFragment extends Fragment implements SwipeHandler {
      */
     private void pullCardsInfo(int batchSize) {
         // TODO: remove placeholder code, pull data from the server
-        Bitmap image = null;
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.default_accommodation_picture);
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.default_accommodation_picture);
+
         for (int i = 0; i < batchSize; i++) {
+            String[] sample = new String[14];
+            Arrays.fill(sample, "text" + i);
+            ArrayList<String> sampleArrList = new ArrayList<>(Arrays.asList(sample));
+
             ArrayList<Bitmap> images = new ArrayList<>();
             images.add(image);
             images.add(image);
-            dataModels.add(new AccommodationInfo(String.format("Building %d", i), String.format("Description %d", i),
-                    images, null, 720));
+
+            dataModels.add(new AccommodationInfo(sampleArrList, images, null));
         }
     }
 
