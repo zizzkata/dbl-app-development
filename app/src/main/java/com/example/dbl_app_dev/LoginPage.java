496f9413f1595d76eae3c4394bb4d8a4f441a95f
Zdravko Zdravkov <z.n.zdravkov@student.tue.nl>
2022-04-03 20:54:33 +0200

    Add polished functionality for login
    Add fully functional login that is also error prone.

--- app/src/main/java/com/example/dbl_app_dev/LoginPage.java
+++ app/src/main/java/com/example/dbl_app_dev/LoginPage.java
@@ -2,20 +2,50 @@ package com.example.dbl_app_dev;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.dbl_app_dev.store.Store;
+import com.example.dbl_app_dev.util.view_validation.scenarios.NetworkLoginErrorScenario;
 
 public class LoginPage extends AppCompatActivity {
+
+    // text boxes
+    private EditText email;
+    private TextView credentialsWarning;
+    private EditText password;
+
+    private TextView loginBtn;
+    private TextView signUpTxt;
+
+    /**
+     * initialise text boxes
+     */
+    private void init() {
+        // Text fields
+        email = findViewById(R.id.editUsernameBox);
+        password = findViewById(R.id.editPasswordBox);
+
+        // Warnings
+        credentialsWarning = findViewById(R.id.invalidCredentials);
+        credentialsWarning.setVisibility(View.GONE);
+
+        // Buttons
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpTxt = findViewById(R.id.signUpTxt);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
+        init();
+
         // Sign Up button leading to RegisterPage
-        TextView signUpTxt = findViewById(R.id.signUpTxt);
         signUpTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,13 +55,44 @@ public class LoginPage extends AppCompatActivity {
         });
 
         // Log in button leading to MainNavigationPage
-        TextView loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginPage.this, MainNavigationActivity.class));
-                overridePendingTransition(0, 0);
+                setCredentialsWarning(false); // hide error message
+                login(view);
             }
         });
+    }
+
+    private void login(View view) {
+        String emailString = email.getText().toString();
+        String passwordString = password.getText().toString();
+
+        if (emailString == "" || passwordString == "") {
+            setCredentialsWarning("Please fill in all fields.", true);
+            return;
+        }
+
+        if (Store.login(emailString, passwordString)) {
+            startActivity(new Intent(LoginPage.this, MainNavigationActivity.class));
+            overridePendingTransition(0, 0);
+        } else {
+            String err = new NetworkLoginErrorScenario(Store.getLastException()).getError();
+            setCredentialsWarning(err, true);
+        }
+    }
+
+    /** Controls for the warning message field */
+    private void setCredentialsWarning(String message, boolean visible) {
+        credentialsWarning.setText(message);
+        credentialsWarning.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    private void setCredentialsWarning(boolean visible) {
+        credentialsWarning.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    private void setCredentialsWarning(String message) {
+        credentialsWarning.setText(message);
     }
 }
