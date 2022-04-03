83b23a10ed5defc28311b23cfd6c378ec7a7c692
Zdravko Zdravkov <z.n.zdravkov@student.tue.nl>
2022-04-04 01:28:12 +0200

    Add Signup method
    Add endpoint for signup. Also add the necessary queries.

--- app/src/main/java/com/example/dbl_app_dev/network_communication/Authentication.java
+++ app/src/main/java/com/example/dbl_app_dev/network_communication/Authentication.java
@@ -1,5 +1,6 @@
 package com.example.dbl_app_dev.network_communication;
 
+import android.util.Log;
 import com.example.dbl_app_dev.util.AsyncWrapper;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -8,10 +9,11 @@ import com.google.firebase.auth.FirebaseUser;
 
 public abstract class Authentication {
 
-    /**  */
+    /**  Firebase Authenticator instance */
     private static FirebaseAuth auth = FirebaseAuth.getInstance();
 
     /**
+     * Login method for Firebase
      *
      * @param email
      * @param password
@@ -22,9 +24,38 @@ public abstract class Authentication {
         return AsyncWrapper.wrap(auth.signInWithEmailAndPassword(email, password));
     }
 
+    /**
+     * Registration method for Firebase,
+     * which has several fail-safe switches implemented.
+     *
+     * @param email
+     * @param password
+     * @param username
+     */
+    public static void firebaseSignup(String email, String password, String username)
+            throws Exception {
+        // Register user
+        AuthResult res = AsyncWrapper.wrap(auth.createUserWithEmailAndPassword(email, password));
 
-    public static void firebaseSignup(String email, String password, String username) {
-        //TODO
+        // Try to create identity file
+        try {
+            AsyncWrapper.wrap(FirebaseQueries.reserveIdentity(email, username
+                    , res.getUser().getUid()));
+        } catch (Exception e) {
+            Log.e("firebaseSignup(): reserveIdentity", e.getMessage());
+            AsyncWrapper.wrapSafe(res.getUser().delete());
+            throw e;
+        }
+
+        // Try to create user file
+        try {
+            AsyncWrapper.wrap(FirebaseQueries.createNewUser(username));
+        } catch (Exception e) {
+            Log.e("firebaseSignup(): createNewUser", e.getMessage());
+            AsyncWrapper.wrapSafe(FirebaseQueries.deleteIdentity(email));
+            AsyncWrapper.wrapSafe(res.getUser().delete());
+            throw e;
+        }
     }
 
     public static FirebaseUser getCurrentUser() {
--- app/src/main/java/com/example/dbl_app_dev/network_communication/FirebaseQueries.java
+++ app/src/main/java/com/example/dbl_app_dev/network_communication/FirebaseQueries.java
@@ -6,22 +6,38 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class FirebaseQueries {
-    /** firebase db */
+    /**
+     * firebase db
+     */
     private static FirebaseFirestore fireStore = FirebaseFirestore
             .getInstance(Authentication.getApp());
 
-    /** db collections */
+    /**
+     * db collections
+     */
     private static CollectionReference users = fireStore.collection("users");
     private static CollectionReference identity = fireStore.collection("identity");
 
     /**
+     * Get batch job from Firestore
+     *
+     * @return Batch
+     */
+    public static WriteBatch getBatch() {
+        return fireStore.batch();
+    }
+
+    /**
      * Get identity snapshot by providing an email
+     *
      * @param email
+     * @return Task<DocumentSnapshot>
      * @pre user needs to be authenticated
-     * @return
      */
     public static Task<DocumentSnapshot> getUsername(String email) {
         Log.d("getUsernameByEmail", "Email: " + email);
@@ -30,8 +46,9 @@ public abstract class FirebaseQueries {
 
     /**
      * Get user snapshot by providing an email
+     *
      * @param username
-     * @return
+     * @return Task<DocumentSnapshot>
      */
     public static Task<DocumentSnapshot> getUserInformation(String username) {
         Log.d("getUserByUsername", "Username: " + username);
@@ -40,14 +57,62 @@ public abstract class FirebaseQueries {
 
     /**
      * Update personal information
+     *
      * @param username
      * @param data
+     * @return Task<Void>
      * @pre user needs to be authenticated
-     * @return
      */
     public static Task updateUserInformation(String username, @NonNull Map data) {
         Log.d("updateUserInformation", "Username: " + username);
         return users.document(username).update(data);
     }
 
+    /**
+     * Create user-tailored file in /identity for authentication.
+     *
+     * @param email
+     * @param username
+     * @param uid
+     * @return Task<Void>
+     */
+    public static Task reserveIdentity(String email, String username, String uid) {
+        Log.d("reserveIdentity", "Email: " + email + " Username: " + username
+                + " UID: " + uid);
+        Map<String, String> identityFile = new HashMap<>();
+        identityFile.put("username", username);
+        identityFile.put("uid", uid);
+        return identity.document(email).set(identityFile);
+    }
+
+    /**
+     * Delete user-tailored file for authentication.
+     *
+     * @param email
+     * @return Task<Void>
+     */
+    public static Task deleteIdentity(String email) {
+        Log.d("deleteIdentity", "Email: " + email);
+        return identity.document(email).delete();
+    }
+
+    /**
+     * Create new User in db.
+     *
+     * @param username
+     * @return Task<Void>
+     */
+    public static Task createNewUser(String username) {
+        Log.d("reserveIdentity", "Username: " + username);
+        // Fill in empty data
+        Map<String, String> userFile = new HashMap<>();
+        userFile.put("first_name", "");
+        userFile.put("last_name", "");
+        userFile.put("age", "");
+        userFile.put("gender", "");
+        userFile.put("phone_number", "");
+        userFile.put("profile_description", "");
+        userFile.put("tenant_mode", "true");
+        return users.document(username).set(userFile);
+    }
 }
