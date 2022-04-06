f72f89de1210a1f472ad5bd7157e757d0bb44fde
Andree34 <52709475+Andree34@users.noreply.github.com>
2022-04-06 18:05:39 +0200

    Add AuthenticationScenario to currPassValidator
    -Also, currPasswordValidator now extends from TextPattenValidator, so that old passwords can be changed to new format (NECESSARY FOR ACCEPTANCE TEST PLAN)

--- app/src/main/java/com/example/dbl_app_dev/util/view_validation/validators/currPasswordValidator.java
+++ app/src/main/java/com/example/dbl_app_dev/util/view_validation/validators/currPasswordValidator.java
@@ -3,9 +3,11 @@ package com.example.dbl_app_dev.util.view_validation.validators;
 import android.view.View;
 import android.widget.TextView;
 
-public final class currPasswordValidator extends PasswordValidator{
-    public currPasswordValidator(View toValidate, TextView warning) {
-        super(toValidate, warning);
+import com.example.dbl_app_dev.util.view_validation.scenarios.AuthenticationScenario;
 
+public final class currPasswordValidator extends TextPatternValidator {
+    public currPasswordValidator(View currPassword, TextView warning) {
+        super(currPassword, warning);
+        scenarios.add(new AuthenticationScenario((TextView) currPassword, "* Incorrect old password"));
     }
 }
