09f00cc91cefb3c5fc80e2f51f20efdfd2811a94
Zdravko Zdravkov <69993150+zizzkata@users.noreply.github.com>
2022-04-08 01:52:47 +0200

    Revert "Settings logic (#19)"
    This reverts commit ee05ec1923a6fb3b9509550152ca5588794c4dbd.

--- app/src/main/java/com/example/dbl_app_dev/RegisterPage.java
+++ app/src/main/java/com/example/dbl_app_dev/RegisterPage.java
@@ -13,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dbl_app_dev.network_communication.Authentication;
 import com.example.dbl_app_dev.util.adapters.TextWatcherAdapter;
-
 import com.example.dbl_app_dev.util.AsyncWrapper;
 import com.example.dbl_app_dev.util.view_validation.constants.Exceptions;
 import com.example.dbl_app_dev.util.view_validation.validators.*;
