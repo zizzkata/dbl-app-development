5290d3287b230bc29a4771754d39427be98e171b
Macovei Tudor <tudor_macovei@iCloud.com>
2022-04-07 20:33:48 +0200

    Clean up SettingsFragment

--- app/src/main/java/com/example/dbl_app_dev/SettingsFragment.java
+++ app/src/main/java/com/example/dbl_app_dev/SettingsFragment.java
@@ -41,16 +41,6 @@ import java.util.ArrayList;
  */
 public class SettingsFragment extends Fragment {
 
-    //User currentUser = new User()
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     // text boxes
     private EditText email;
     private TextView emailWarning;
@@ -171,20 +161,9 @@ public class SettingsFragment extends Fragment {
         return areValidatorsValid;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment thirdFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
+    public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -192,11 +171,6 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
     }
 
     @Override
@@ -214,7 +188,7 @@ public class SettingsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         init();
-        //makeWarningsInvisible();
+//         makeWarningsInvisible();
         AsyncWrapper.wrap(() -> {
             User user;
             try {
@@ -234,7 +208,7 @@ public class SettingsFragment extends Fragment {
                             .getAssets().open("default-user-image.png");
                     profilePic = BitmapFactory.decodeStream(stream);
                 }
-                Bitmap finalProfilePic = profilePic; // necessary for javalint to run ;/
+                Bitmap finalProfilePic = profilePic; // necessary for java lint to run ;/
                 getActivity().runOnUiThread(() -> bindUserImage(finalProfilePic));
             } catch (Exception e) { // never happens
                 Log.e("FATAL", e.getMessage());
@@ -243,12 +217,10 @@ public class SettingsFragment extends Fragment {
         });
 
         // Sign Up button leading to RegisterPage
-        TextView signUpTxt = getView().findViewById(R.id.logoutBtn);
-        signUpTxt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        TextView logOutBtn = getView().findViewById(R.id.logoutBtn);
+        logOutBtn.setOnClickListener(view1 -> {
+//            ((MainNavigationActivity) getActivity()).viewAccommodationDialog(view1);
                 startActivity(new Intent(getActivity(), LoginPage.class));
-            }
         });
 
         // Save button used to update current user's personal info and password
@@ -284,6 +256,19 @@ public class SettingsFragment extends Fragment {
                     }
                 });
             }
+
+            try {
+                updateUserSettings();
+            } catch (Exception ignored) {
+                // exceptions are ignored
+            }
+
+            Context context = getActivity().getApplicationContext();
+            String text = "ok";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         });
     }
 
