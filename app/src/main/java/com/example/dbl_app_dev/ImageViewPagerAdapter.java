00060af4c213717dea5b8254876473acfbae848d
Macovei Tudor <tudor_macovei@iCloud.com>
2022-04-08 12:42:41 +0200

    Fix tenant discovery (#24)
    * Fix compilation error
    
    * Fix tenant discovery page
    
    * Fix id update to use size of images
    
    * Fix bug showing default UI on view switch

--- app/src/main/java/com/example/dbl_app_dev/ImageViewPagerAdapter.java
+++ app/src/main/java/com/example/dbl_app_dev/ImageViewPagerAdapter.java
@@ -1,24 +1,28 @@
 package com.example.dbl_app_dev;
 
 import android.graphics.Bitmap;
+import android.os.Parcelable;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
 
 import java.util.ArrayList;
 
 public class ImageViewPagerAdapter extends FragmentPagerAdapter {
-    ArrayList<Bitmap> imageBitmaps;
-    Bitmap panoramicBitmap;
+    private final ArrayList<Bitmap> imageBitmaps;
+    private final Bitmap panoramicBitmap;
+    private long baseId = 0;
 
     public ImageViewPagerAdapter(FragmentManager fm, ArrayList<Bitmap> imageBitmaps, @NonNull Bitmap panoramicBitmap) {
         super(fm);
         this.panoramicBitmap = panoramicBitmap;
         this.imageBitmaps = imageBitmaps;
         Log.d("extra_debug", "ImageViewPagerAdapter Created");
+        Log.d("test", ((Integer) imageBitmaps.size()).toString());
     }
 
     @Override
@@ -32,6 +36,7 @@ public class ImageViewPagerAdapter extends FragmentPagerAdapter {
         if (position == 0) {
             return ARGalleryFragment.newInstance(panoramicBitmap);
         } else {
+            Log.d("test", "creating new image fragment");
             return ImageGalleryFragment.newInstance(imageBitmaps.get(position - 1));
         }
     }
@@ -39,5 +44,22 @@ public class ImageViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         return "Child Fragment " + position;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        // refresh all fragments when data set changed
+        return PagerAdapter.POSITION_NONE;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // give an ID different from position when position has been changed
+        return baseId + position;
+    }
+
+    public void notifyChangeInPosition(int n) {
+        // shift the ID returned by getItemId outside the range of all previous fragments
+        baseId += getCount() + n;
     }
 }
