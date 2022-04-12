dc698fbef9479b3889af072e90f4a676194296e1
Lunchador <n2dxdevvil@gmail.com>
2022-04-12 16:32:17 +0200

    Restructure LandlordDiscoverFragment class

--- app/src/main/java/com/example/dbl_app_dev/LandlordDiscoverFragment.java
+++ app/src/main/java/com/example/dbl_app_dev/LandlordDiscoverFragment.java
@@ -37,9 +37,17 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
 
     private LinkedList<TenantInfo> tenantInfo;
     private GestureDetector swipeListener;
-    private TenantInfo currentTenantInfo = null; // currently viewed tenant
+    private User currentTenantInfo; // currently viewed tenant
     ConstraintLayout noSwipesContainer;
     ConstraintLayout contentContainer;
+
+    private TextView nameTxt;
+    private TextView emailTxt;
+    private TextView phoneNumberTxt;
+    private TextView petsTxt;
+    private TextView smokerTxt;
+    private TextView descriptionTxt;
+    private ImageView imageView;
 
     public LandlordDiscoverFragment() {
         // Required empty public constructor
@@ -58,8 +66,9 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tenantInfo = new LinkedList<>();
-        pullCardsInfo(10);
+        // Will probably remove this
+        //tenantInfo = new LinkedList<>();
+        //pullCardsInfo(10);
     }
 
     @Override
@@ -76,15 +85,21 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
 
         ConstraintLayout topCard = view.findViewById(R.id.topCard);
 
-        ImageView imageView = view.findViewById(R.id.tenantPicture);
-
-        ArrayList<TextView> cardTextViews = new ArrayList<>();
+        /*ArrayList<TextView> cardTextViews = new ArrayList<>();
         cardTextViews.add(view.findViewById(R.id.nameTxt));
         cardTextViews.add(view.findViewById(R.id.emailTxt));
         cardTextViews.add(view.findViewById(R.id.phoneNumberTxt));
         cardTextViews.add(view.findViewById(R.id.petsTxt));
         cardTextViews.add(view.findViewById(R.id.smokerTxt));
-        cardTextViews.add(view.findViewById(R.id.descriptionTxt));
+        cardTextViews.add(view.findViewById(R.id.descriptionTxt));*/
+
+        nameTxt = view.findViewById(R.id.nameTxt);
+        emailTxt = view.findViewById(R.id.emailTxt);
+        phoneNumberTxt = view.findViewById(R.id.phoneNumberTxt);
+        petsTxt = view.findViewById(R.id.petsTxt);
+        smokerTxt = view.findViewById(R.id.smokerTxt);
+        descriptionTxt = view.findViewById(R.id.descriptionTxt);
+        imageView = view.findViewById(R.id.tenantPicture);
 
         noSwipesContainer = view.findViewById(R.id.noSwipesContainer);
         contentContainer = view.findViewById(R.id.contentContainer);
@@ -92,16 +107,18 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
         contentContainer.setVisibility(View.VISIBLE);
 
         // makes sure that the a card is not discarded if it is not rated
-        if (currentTenantInfo == null) {
-            nextCard(cardTextViews, imageView);
-        } else {
-            displayCard(cardTextViews, imageView);
-        }
+        //if (currentTenantInfo == null) {
+        //    nextCard(cardTextViews, imageView);
+        //} else {
+        //    displayCard(cardTextViews, imageView);
+        //}
 
         this.swipeListener = new GestureDetector(getContext(), new CardSwipeListener(this, true, true));
         topCard.setOnTouchListener((v, event) -> {
             if (swipeListener.onTouchEvent(event)) {
-                nextCard(cardTextViews, imageView);
+                displayCard(true);
+                Log.i("wafaw", "wgagaw");
+                return false;
             }
             return true;
         });
@@ -109,34 +126,52 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
         Button likeBtn = view.findViewById(R.id.positiveButton);
         Button dislikeBtn = view.findViewById(R.id.negativeButton);
         Button neutralBtn = view.findViewById(R.id.neutralButton);
+
         likeBtn.setOnClickListener(v -> {
             swipedRight();
-            nextCard(cardTextViews, imageView);
+            displayCard(true);
         });
         dislikeBtn.setOnClickListener(v -> {
             swipedLeft();
-            nextCard(cardTextViews, imageView);
+            displayCard(true);
         });
         neutralBtn.setOnClickListener(v -> {
             swipedDown();
-            nextCard(cardTextViews, imageView);
+            displayCard(true);
         });
+
+        displayCard(currentTenantInfo == null);
+
     }
 
     /**
      * Displays the information stored in currentAccommodationInfo
      */
-    private void displayCard(ArrayList<TextView> cardTextViews, ImageView cardImage) {
-        ArrayList<String> cardStrings = currentTenantInfo.getFormattedText();
+    private void displayCard(boolean nextCard) {
+        /*ArrayList<String> cardStrings = currentTenantInfo.getFormattedText();
 
         assert (cardStrings.size() == cardTextViews.size()) : "Incorrect size of tenant info strings";
         for (int i = 0; i < cardStrings.size(); i++) {
             cardTextViews.get(i).setText(cardStrings.get(i));
         }
-        cardImage.setImageBitmap(currentTenantInfo.getPhoto());
+        cardImage.setImageBitmap(currentTenantInfo.getPhoto());*/
+        AsyncWrapper.wrap(() -> {
+            try {
+                if (nextCard) {
+                    currentTenantInfo = Store.getCurrentUser();
+                }
+                getActivity().runOnUiThread(() -> {
+                    bindData(currentTenantInfo);
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.i("I shit myself", e.getMessage());
+            }
+        });
     }
 
-    private void nextCard(ArrayList<TextView> cardTitle, ImageView cardImage) {
+    /*private void nextCard(ArrayList<TextView> cardTitle, ImageView cardImage) {
         if (tenantInfo.size() > 0) {
             currentTenantInfo = this.tenantInfo.remove();
             displayCard(cardTitle, cardImage);
@@ -146,30 +181,15 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
             noSwipesContainer.setVisibility(View.VISIBLE);
             contentContainer.setVisibility(View.INVISIBLE);
         }
-    }
+    }*/
 
     /**
      * Pulls tenant data from server, adds it to tenantInfo
      *
      * @param batchSize number of cards to add to the tenantInfo queue
      */
-    private void pullCardsInfo(int batchSize) {
+    /*private void pullCardsInfo(int batchSize) {
         AsyncWrapper.wrap(() -> {
-            /*try {
-                User currentUser = Store.getCurrentUser();
-                Bitmap cuImage = currentUser.getProfilePic();
-                String[] name = new String[6];
-                name[0] = currentUser.getFirstName() + " " + currentUser.getFirstName();
-                name[1] = "bulgari unaci";
-                name[2] = "bulgari unaci";
-                name[3] = "bulgari unaci";
-                name[4] = "bulgari unaci";
-                name[5] = "bulgari unaci";
-                tenantInfo.add(new TenantInfo(new ArrayList<>(Arrays.asList(name)), cuImage));
-            } catch (Exception e) {
-                Log.i("getCurrentUserDiscover", e.getMessage());
-            }*/
-
             try {
                 User currentUser = Store.getCurrentUser();
                 ArrayList<User> ratedTenants = Database.getRatedTenants(currentUser.getUsername());
@@ -188,16 +208,16 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
                 Log.i("Port user to discovery component", e.getMessage());
             }
         });
-    }
+    }*/
 
     /**
      * POST's the positive rating given to the viewed tenant to the backend
      */
     @Override
     public void swipedRight() {
-        if (tenantInfo.size() > 0) {
+        //if (tenantInfo.size() > 0) {
             Log.i("extra_debug", "Positive Rating");
-        }
+        //}
     }
 
     /**
@@ -205,9 +225,9 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
      */
     @Override
     public void swipedLeft() {
-        if (tenantInfo.size() > 0) {
+        //if (tenantInfo.size() > 0) {
             Log.i("extra_debug", "Negative Rating");
-        }
+        //}
     }
 
     /**
@@ -215,8 +235,26 @@ public class LandlordDiscoverFragment extends Fragment implements SwipeHandler {
      */
     @Override
     public void swipedDown() {
-        if (tenantInfo.size() > 0) {
+        //if (tenantInfo.size() > 0) {
             Log.i("extra_debug", "Neutral Rating");
+        //}
+    }
+
+    public void bindData(User data) {
+        nameTxt.setText(data.getFirstName() + " " + data.getLastName());
+        emailTxt.setText("Username: " + data.getUsername());
+        phoneNumberTxt.setText("Phone number: " + data.getPhoneNumber());
+        petsTxt.setText("Pet owner: " + booleanToYesNo(data.hasPets()));
+        smokerTxt.setText("Smoker: " + booleanToYesNo(data.smokes()));
+        descriptionTxt.setText(data.getDescription());
+        imageView.setImageBitmap(data.getProfilePic());
+    }
+
+    private String booleanToYesNo(boolean x) {
+        if (x) {
+            return "Yes";
+        } else {
+            return "No";
         }
     }
 
